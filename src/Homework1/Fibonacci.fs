b73225a3e8641a15aa399cbdece767826bbedb53
ArtiomPatov <patov.988@gmail.com>
2023-03-04 17:38:58 +0300

    implement fibonacci

---
+++ src/Homework1/Fibonacci.fs
@@ -0,0 +1,8 @@
+﻿module Homework1.Fibonacci
+
+let Fibonacci n =
+    let fibonacciSequence = (0, 1)
+                            |> Seq.unfold (fun (a, b) -> Some(a + b, (b, a + b)))
+                            |> Seq.take n
+
+    Seq.append [0; 1] fibonacciSequence

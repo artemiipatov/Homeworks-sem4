a62296f52142828a3b980d818b167c1f6f312a3d
ArtiomPatov <patov.988@gmail.com>
2023-03-10 10:20:18 +0300

    edit build.fs; refactor

--- src/Homework1/Fibonacci.fs
+++ src/Homework1/Fibonacci.fs
@@ -10,6 +10,13 @@ module Fibonacci =
     /// <param name="n">Length of the sequence.</param>
     let fibonacci n =
         let fibonacciSequence =
-            (0, 1) |> Seq.unfold (fun (a, b) -> Some(a + b, (b, a + b))) |> Seq.take n
+            (0, 1)
+            |> Seq.unfold (fun (a, b) -> Some(a + b, (b, a + b)))
+            |> Seq.take n
 
-        Seq.append [ 0; 1 ] fibonacciSequence
+        Seq.append
+            [
+                0
+                1
+            ]
+            fibonacciSequence

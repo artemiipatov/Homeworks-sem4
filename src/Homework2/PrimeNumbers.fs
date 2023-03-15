e84da5fdc7901d2bc6aa237601e1321fb5ba2dd1
ArtiomPatov <patov.988@gmail.com>
2023-03-15 14:02:06 +0300

    add prime numbers

---
+++ src/Homework2/PrimeNumbers.fs
@@ -0,0 +1,16 @@
+﻿namespace Homework2
+
+module PrimeNumbers =
+    let isPrime n =
+
+        let sqrt =
+            float
+            >> sqrt
+            >> int
+
+        [ 2 .. sqrt n ]
+        |> List.forall (fun v -> n % v <> 0)
+
+    let initSeqOfPrimeNumber =
+        (Seq.initInfinite id)
+        |> Seq.filter isPrime

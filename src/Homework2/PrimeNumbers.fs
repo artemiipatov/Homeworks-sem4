da621089150162da41fb92f388a0d83e4335b4e1
ArtiomPatov <patov.988@gmail.com>
2023-03-15 14:06:56 +0300

    refactor

--- src/Homework2/PrimeNumbers.fs
+++ src/Homework2/PrimeNumbers.fs
@@ -1,16 +1,23 @@
 ﻿namespace Homework2
 
+/// <summary>
+/// Module that contains function for initializing infinite sequence of prime numbers.
+/// </summary>
 module PrimeNumbers =
+    /// <summary>
+    /// Checks if number is prime.
+    /// </summary>
+    /// <param name="n">Input number.</param>
     let isPrime n =
 
-        let sqrt =
-            float
-            >> sqrt
-            >> int
+        let sqrt = float >> sqrt >> int
 
         [ 2 .. sqrt n ]
         |> List.forall (fun v -> n % v <> 0)
 
+    /// <summary>
+    /// Initializes infinite sequence of prime numbers.
+    /// </summary>
     let initSeqOfPrimeNumber =
         (Seq.initInfinite id)
         |> Seq.filter isPrime

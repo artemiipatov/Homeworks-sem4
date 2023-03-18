153f437e09232971da88305b25f51ffb37c655e8
ArtiomPatov <patov.988@gmail.com>
2023-03-18 21:41:50 +0300

    add parse tree

---
+++ src/Homework2/ParseTree.fs
@@ -0,0 +1,21 @@
+﻿namespace Homework2
+
+module ParseTree =
+
+    type NodeType<'a> =
+        | Number of 'a
+        | Operation of ('a -> 'a -> 'a) * NodeType<'a> * NodeType<'a>
+
+        member this.eval =
+            match this with
+            | Number n -> n
+            | Operation(op, left, right) -> (op left.eval right.eval)
+
+    type Tree<'b> =
+        | Node of NodeType<'b>
+        | Empty
+
+    let eval tree =
+        match tree with
+        | Node node -> Some node.eval
+        | Empty -> None

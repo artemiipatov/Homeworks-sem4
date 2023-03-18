41edd5a349231d123506a4fa5621391baccebf6c
ArtiomPatov <patov.988@gmail.com>
2023-03-18 19:37:49 +0300

    add tree map

---
+++ src/Homework2/Tree.fs
@@ -0,0 +1,15 @@
+﻿namespace Homework2
+
+type Tree<'a> =
+    | Node of value: 'a * left: Tree<'a> * right: Tree<'a>
+    | Empty
+
+module Tree =
+
+    let rec map op tree =
+
+        match tree with
+        | Node(value, left, right) ->
+            (op value, map op left, map op right)
+            |> Node
+        | Empty -> Empty

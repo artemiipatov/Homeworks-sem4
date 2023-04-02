a7c8b207d12f13ee284c1fc51ef3a0aa3cf09cb5
artemiipatov <patov.988@gmail.com>
2023-04-02 13:12:04 +0300

    refactor: add comments

--- src/Homework2/Interpreter.fs
+++ src/Homework2/Interpreter.fs
@@ -1,15 +1,24 @@
 ﻿namespace Homework2
 
+/// Lambda interpreter, which supports beta reduction and automatic alpha conversion.
 module Interpreter =
+    /// Represents variable name.
     type VarName = string
 
+    /// Represents lambda term.
     type Term =
         | Var of VarName
         | Abs of VarName * Term
         | App of Term * Term
 
-    let rec replace mainTerm oldTerm newTerm =
-        match mainTerm with
+    /// <summary>
+    /// Replaces all occurrences of the one term with another.
+    /// </summary>
+    /// <param name="expression">Lambda expression where terms are replaced.</param>
+    /// <param name="oldTerm">Term that is being replaced.</param>
+    /// <param name="newTerm">New term.</param>
+    let rec replace expression oldTerm newTerm =
+        match expression with
         | App(term1, term2) -> App(replace term1 oldTerm newTerm, replace term2 oldTerm newTerm)
         | Abs(var, term) ->
             match newTerm with
@@ -20,18 +29,32 @@ module Interpreter =
             | _ -> Abs(var, replace term oldTerm newTerm)
         | Var var -> if var = oldTerm then newTerm else var |> Var
 
+    /// <summary>
+    /// Renames given bound variable.
+    /// </summary>
+    /// <param name="absBody">Abstraction body in which renaming take place.</param>
+    /// <param name="oldName">Name of the bound variable to be renamed.</param>
     and renameBoundVar absBody (oldName: VarName) =
         let newName = ((oldName |> string) + "'") |> VarName
         let newVar = newName |> Var
         let newTerm = replace absBody oldName newVar
         Abs(newName, newTerm)
 
+    /// <summary>
+    /// Applies one term to another.
+    /// </summary>
+    /// <param name="mainTerm">Term to which apply another term.</param>
+    /// <param name="applicableTerm">Term to apply.</param>
     let rec apply mainTerm applicableTerm =
         match mainTerm with
         | App _ -> App(mainTerm, applicableTerm)
         | Abs(var, term) -> replace term var applicableTerm
         | Var var -> var |> Var
 
+    /// <summary>
+    /// Converts given lambda expression to beta normal form.
+    /// </summary>
+    /// <param name="expression"></param>
     let reduce expression =
         let rec reduceRec exp =
             match exp with

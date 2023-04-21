c164d9e15ebbf56c2f9bda606cb10d468ecc1821
artemiipatov <patov.988@gmail.com>
2023-04-21 13:00:27 +0300

    fix: tests

--- tests/Homeworks.Tests/Main.fs
+++ tests/Homeworks.Tests/Main.fs
@@ -3,8 +3,7 @@ open Homeworks.Tests
 open Homeworks.Tests.Test1
 
 [<Tests>]
-let allTests = testList "all Tests" [ Supermap.tests
-                                      Rhombus.generalTests ]
+let allTests = testList "all Tests" [ Supermap.tests; Rhombus.generalTests ]
 
 [<EntryPoint>]
 let main argv =
--- tests/Homeworks.Tests/Supermap.fs
+++ tests/Homeworks.Tests/Supermap.fs
@@ -3,34 +3,28 @@
 open Expecto
 open Test1.Supermap
 
-let createTest mapFunList valueList expected testName =
-    test testName {
-        let actual: int list = run valueList mapFunList
-        Expect.equal actual expected "Results must be the same"
-    }
+let createTest mapFunList valueList expected =
+    let actual: int list = run valueList mapFunList
+    Expect.equal actual expected "Results must be the same"
 
 let testCommonMap valueList =
-    [ let mapFun = (*) 3
-      let expected = valueList |> List.map mapFun
-      createTest [ mapFun ] valueList expected "Common map" ]
-    |> testList "Common map"
+    let mapFun = (*) 3
+    let expected = valueList |> List.map mapFun
+    createTest [ mapFun ] valueList expected
 
 let generalTests (valueList, coefficients) =
-    [ let mapFun = coefficients |> List.map (fun x -> (*) x)
+    let mapFun = coefficients |> List.map (fun x -> (*) x)
 
-      let expected: int list =
-          let rec calculate values mappedValues =
-              match values with
-              | [] -> mappedValues
-              | v :: tail ->
-                  calculate
-                      tail
-                      (mappedValues @ (mapFun |> List.map (fun func -> func v)))
+    let expected: int list =
+        let rec calculate values mappedValues =
+            match values with
+            | [] -> mappedValues
+            | v :: tail ->
+                calculate tail (mappedValues @ (mapFun |> List.map (fun func -> func v)))
 
-          calculate valueList []
+        calculate valueList []
 
-      createTest mapFun valueList expected "Many functions list" ]
-    |> testList "General"
+    createTest mapFun valueList expected
 
 let tests =
     [ testCommonMap
